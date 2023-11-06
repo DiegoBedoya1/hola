8fb13385f1dec2a6cf70b971a0a4756083d9d612
Diego Bedoya <dbedoya@espol.edu.ec>
2023-11-05 19:37:19 -0500

    esto es solo una prueba

---
+++ prueba/src/main/java/com/pooespol/prueba/Prueba.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pooespol.prueba;
+
+/**
+ *
+ * @author diego
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("esto es una prueba");    
+    
+    }
+}
